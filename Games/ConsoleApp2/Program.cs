82009a57b8473c531267d86a98e8dd6704bcb7de
kamiljanek <gttony@o2.pl>
2022-07-10 17:19:33 +0200

    Add new hello  7 8 9 line

--- Games/ConsoleApp2/Program.cs
+++ Games/ConsoleApp2/Program.cs
@@ -33,6 +33,9 @@ namespace RequestsTest
             Console.WriteLine("hello 4");
             Console.WriteLine("hello 5");
             Console.WriteLine("hello 6");
+            Console.WriteLine("hello 7");
+            Console.WriteLine("hello 8");
+            Console.WriteLine("hello 9");
 
 
 
